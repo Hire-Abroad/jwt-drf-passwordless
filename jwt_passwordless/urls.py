343f508904a679975a5dd97d3ac46bfee994407e
abood-74 <abdelrahmanashour45@gmail.com>
2025-04-09 02:32:09 +0200

    Refactor URL patterns to remove unused JWT token refresh and blacklist endpoints

--- jwt_passwordless/urls.py
+++ jwt_passwordless/urls.py
@@ -5,15 +5,11 @@ from jwt_passwordless.views import (
      ObtainMobileCallbackToken,
      ObtainAuthTokenFromCallbackToken,
 )
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenBlacklistView
-
 
 app_name = 'jwt_passwordless'
 
 urlpatterns = [
      path(api_settings.PASSWORDLESS_AUTH_PREFIX + 'email/', ObtainEmailCallbackToken.as_view(), name='auth_email'),
      path(api_settings.PASSWORDLESS_AUTH_PREFIX + 'mobile/', ObtainMobileCallbackToken.as_view(), name='auth_mobile'),
-     path(api_settings.PASSWORDLESS_AUTH_PREFIX + 'token/', ObtainAuthTokenFromCallbackToken.as_view(), name='auth_token'),
-     path('jwt/refresh/', TokenRefreshView.as_view(), name='token_refresh'), 
-     path('jwt/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),   
+     path(api_settings.PASSWORDLESS_AUTH_PREFIX + 'token/', ObtainAuthTokenFromCallbackToken.as_view(), name='auth_token') 
 ]
