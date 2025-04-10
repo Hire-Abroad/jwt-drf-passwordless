5d9035c480b127cbf2772a957eea171a7d78226b
yaaa3ser <yasserissa1411@gmail.com>
2025-04-10 11:15:37 +0200

    chore: update version to 1.0.1 and add copyright year in LICENSE

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ URL = 'https://github.com/Hire-Abroad/jwt-passwordless'
 EMAIL = 'contact@hireabroad.com'
 AUTHOR = 'Hire Abroad'
 REQUIRES_PYTHON = '>=3'
-VERSION = '1.0.0'
+VERSION = '1.0.1'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
